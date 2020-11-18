8ec5b3d2fafe951d15900844c85647b6c69e2f66
Marco Arthur <arthurpbs@gmail.com>
2020-11-18 15:48:00 -0300

    Fix
    Fix data importing

--- data-raw/import_contracts.R
+++ data-raw/import_contracts.R
@@ -8,19 +8,15 @@ tipos <- read_csv("inst/tipo_contrato.csv")
 names(tipos) <- c("codigo", "tipo_contrato")
 modalidades <- read_csv("inst/modalidades.csv")
 names(modalidades) <- c("codigo", "modalidade")
-# from 2000 to date
-compras_contracts <- read_csv("inst/contracts.csv") %>%
-    filter( data_assinatura > '2000-01-01')
 
-# replacing id by label
-compras_contracts <-
-    compras_contracts %>%
+# from 2000 to date and replacing id by label
+# removing unwanted columns and auxiliary datasets
+compras_contracts <- compras_contracts <- read_csv("inst/contracts.csv") %>%
+    filter( data_assinatura > '2000-01-01') %>%
     inner_join( modalidades, by = c( "modalidade_licitacao" = "codigo")) %>% 
-    inner_join( tipos, by = c( "codigo_contrato" = "codigo"))
-
-# remove unwanted columns and auxiliary datasets
-compras_contracts <- compras_contracts %>%
+    inner_join( tipos, by = c( "codigo_contrato" = "codigo")) %>%
     select( -cnpj_contratada, -modalidade_licitacao , -codigo_contrato, -codigo_contrato )
+
 rm(tipos, modalidades)
 
 # export data
